75c4ccd436ed26e66509533452ddf3b416edfe6a
Crusader0013 <rouzbehmalekahmadi@gmail.com>
2024-01-05 22:17:37 +0330

    last changes

--- roozi.py
+++ roozi.py
@@ -25,12 +25,12 @@ class Poeple :
     def __init__(self) : 
         self.users = {}
 
-    def sign_in(self , email , name , user_type ):
+    def sign_in(self , email , name , national_code , age , insurance , sickness , user_type ):
         if name not in self.users :
-            self.users[name] = User(email , name , user_type , generate_username(5) , generate_password(10))
+            self.users[name] = User(email , name , national_code , age , insurance , sickness , user_type , generate_username(5) , generate_password(10))
         else : 
             raise Exception ("This name is already exist in our database")
-    def log_in(self , username , password) : 
+    def log_in(self , username , password ) : 
         pass
 
     def refresh_profile(self , name , username , password) :
@@ -41,9 +41,13 @@ class Poeple :
 
 class User :
 
-    def __init__(self , email , name , user_type , username , password):
+    def __init__(self , email , name , national_code , age , insurance , sickness , user_type , username , password):
         self.name = name
         self.email = email
+        self.national_code = national_code
+        self.age = age
+        self.insurance = insurance
+        self.sickness = sickness
         self.user_type = user_type
         self.username = username
         self.password = password
