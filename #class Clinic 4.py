4e508857cd05ecf4239298eefb47f0d8c4bcf4f8
Pardis <rahiminasrpardis@gmail.com>
2024-01-12 19:05:56 +0330

    modifying class clinic

---
+++ #class Clinic 4.py
@@ -0,0 +1,15 @@
+
+
+class Clinic:
+    
+
+    def __init__(self, clinic_id, address, clinic_contact_info, clinic_password, capacity, service):
+        self.clinic_id = clinic_id
+        self.address = address
+        self.clinic_contact_info = clinic_contact_info
+        self.clinic_password = clinic_password
+        self.capacity = capacity
+        self.service = service
+        
+
+    
