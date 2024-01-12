c583836a080a1af4baa787cadb7d9a51002f5a6f
Crusader0013 <rouzbehmalekahmadi@gmail.com>
2024-01-12 13:36:41 +0330

    deleting people class and make it easier to understand.

--- roozi.py
+++ roozi.py
@@ -18,28 +18,7 @@ class People :
 
     def __init__(self , connection) : 
         self.connection = connection
-        
-    def sign_in_patient(self , patient_national_code , patient_name , patient_contact_info , age , insurance , illness_history , one_time_password , appointment_obj , connection) :
-        if patient_name not in People.all_people :
-            if one_time_password == 'No' : 
-                People.all_people[patient_name] = Patient(patient_national_code , patient_name , patient_contact_info , age , insurance , illness_history , generate_password(10) , 'No' , appointment_obj , connection)
-            elif one_time_password == 'Yes' : 
-                People.all_people[patient_name] = Patient(patient_national_code , patient_name , patient_contact_info , age , insurance , illness_history , generate_password(10) , 'Yes' , appointment_obj , connection)
-        else : 
-            print('This name is already exist in our database')
 
-    def log_in_patient(self , patient_name , password) : 
-        if patient_name in People.all_people : 
-            if People.all_people[patient_name].one_time_password == 'No':
-                if People.all_people[patient_name].password == password : 
-                    print('login succesfully')
-                else : 
-                    print('password is wrong')
-            elif People.all_people[patient_name].one_time_password == 'Yes':
-                print(f'Your one-time password is {generate_password(10)}')
-                print('Login succesfully')
-        else : 
-            print('This name does not exist in our database')
     # a get_info function should be created, returning a dictionary of all the necessary info like the other classes
     def insert_data_patient(self):
         data = self.get_info()
@@ -51,33 +30,10 @@ class People :
                 """
                 cursor.execute(sql , (data['patient_natinal_code'] , data['patient_name'] , data['patient_contact_info'] , data['age'] , data['insurance'] , data['illness_history']))
             self.connection.commit()
-    
-    def sign_in_secretary(self , secretary_name , clinic_id , clinic_password , appointment):
-        if secretary_name in People.all_people : 
-            print("this name is already exist in our database")
-        else :
-            People.all_people[secretary_name] = Secretary(secretary_name , clinic_id , clinic_password , appointment)
-        
-    def log_in_secretary(self , secretary_name , clinic_id , clinic):
-        self.clinic = Clinic.clinic_password
-        if secretary_name not in People.all_people : 
-            print("the username/secretary name is wrong")
-        elif clinic_password != People.all_people[clinic_id].clinic_password : 
-            print("password is wrong")
-        else : 
-            print("login succesfully")
-
-    def sign_in_clinic(self , clinic_id , address , clinic_contact_info , clinic_password , capacity , service , connection) : 
-        if clinic_id in People.all_people : 
-            print("this clinic id is already exist in our database")
-        else : 
-            People.all_people[clinic_id] = Clinic(clinic_id , address , clinic_contact_info , clinic_password , capacity , service , connection)           
-        
-
-
-
 
 class Patient :
+
+    all_patient = {}
 
     def __init__(self , patient_natinal_code , patient_name , patient_contact_info , age , insurance , illness_history , fixed_password , one_time_password , appointment_obj , connection):
         self.patient_natinal_code = patient_natinal_code
@@ -90,6 +46,29 @@ class Patient :
         self.one_time_password = one_time_password
         self.appointment_obj = appointment_obj  # This should be an instance of the Appointment class
         self.connection = connection
+
+    def sign_in(self , patient_national_code , patient_name , patient_contact_info , age , insurance , illness_history , one_time_password , appointment_obj , connection) :
+        if patient_national_code not in Patient.all_patient :
+            if one_time_password == 'No' : 
+               Patient.all_patient[patient_national_code] = Patient(patient_national_code , patient_name , patient_contact_info , age , insurance , illness_history , generate_password(10) , 'No' , appointment_obj , connection)
+            elif one_time_password == 'Yes' : 
+                Patient.all_patient[patient_national_code] = Patient(patient_national_code , patient_name , patient_contact_info , age , insurance , illness_history , generate_password(10) , 'Yes' , appointment_obj , connection)
+        else : 
+            print('This name is already exist in our database')
+
+    def log_in(self , patient_national_code , password) : 
+        if patient_national_code in Patient.all_patient : 
+            if Patient.all_patient[patient_national_code].one_time_password == 'No':
+                if Patient.all_patient[patient_national_code].password == password : 
+                    print('login succesfully')
+                else : 
+                    print('password is wrong')
+            elif Patient.all_patient[patient_national_code].one_time_password == 'Yes':
+                print(f'Your one-time password is {generate_password(10)}')
+                print('Login succesfully')
+        else : 
+            print('This name does not exist in our database')
+
     # appointment_obj here is an istanse of class appointment:
     def book_appointment(self , appointment_obj):
         return appointment_obj.add_appointment(self.name)
@@ -125,11 +104,10 @@ class Patient :
             "patient_illness_history" : self.illness_history,
             "patient_fixed_password" : self.fixed_password,
             "patient_one_time_password" : self.one_time_password,
-            "patient_appointment_obj" : self.appointment_obj,
-            "patient_connection" : self.connection
         }
-
-
+#sign in and login patients with using sign_in and log_in methods using this test object that created in the code :
+#example : p.sign_in("bla bla bla" ... )
+p = Patient("0000000000" , "test" , "00000000" , "0" , "0" , "0" , "yes" , "0000" ,"0" , "0")
 
 connection = pymysql.connect(host = 'localhost' ,
                              user = 'root' ,
