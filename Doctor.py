a3345bf2eb768da4298c41aaa37cc1764c65e727
Pardis <rahiminasrpardis@gmail.com>
2024-01-13 22:07:36 +0330

    main scenario is written uncompletely

--- Doctor.py
+++ Doctor.py
@@ -1,4 +1,5 @@
 #class doctor
+from Patient import Patient
 class Doctor:
     def __init__(self):
       self.doctor_special_code = 'd7654321_doc*'
@@ -6,19 +7,22 @@ class Doctor:
       #here you should add another atribute to make a dictionary for referring each clinic id to each table.
 
     def enter_code(self):
-      code = input("Please enter the doctor special code: ")
-      if code == self.doctor_special_code:
-         return True
-      else:
-         return print('The doctor special code is wrong.')
+      while True:
+        code = input("Please enter the doctor special code: ")
+        if code == self.doctor_special_code:
+          return True
+        else:
+          print('The doctor special code is wrong.')
+          return False
       
     def choose_clinic(self):
-      clinic_id = int(input('Please choose your clinic id.'))
-      if clinic_id in self.clinic_ids:
-         return clinic_id
-      else:
-         return print('No clinic exists with this id.')
-      
+        while True:
+            clinic_id = int(input('Please choose your clinic id.'))
+            if clinic_id in self.clinic_ids:
+                return clinic_id
+            else:
+                print('No clinic exists with this id. Please try again.')
+         
       
     def execute_query(self, query, values=None):
        try:
@@ -31,18 +35,55 @@ class Doctor:
        except Exception as e:
             print(f"An error occurred: {e}")
 
-    def select_each_clinic_info(self, clinic_id):
-        query = f"""
-        SELECT  clinic_id, service, capacity, reserved_appointments 
-        FROM clinics 
-        WHERE clinic_id = ? 
-        ORDER BY clinic_id
-        """
-        values = (clinic_id,)
-        return self.execute_query(query, values)
 
-doctor = Doctor()
-if doctor.enter_code():
-    clinic_id = doctor.choose_clinic()
-    clinic_info = doctor.select_each_clinic_info(clinic_id)
-    print(clinic_info)
+        #if the join is needed 
+    def select_each_clinic_info(self, clinic_id):
+            query = """
+            SELECT 
+            patient_national_code, 
+            patient_name, 
+            patient_contact_info, 
+            patient_age, 
+            patient_insurance, 
+            patients_reserved_appointments
+            FROM patients 
+            WHERE clinic_id = ? AND 
+            patients_reserved_appointments > 0 
+            """
+
+            values = (clinic_id,)
+            return self.execute_query(query, values)
+    
+    def view_patient_data(self):
+        
+          patient_national_code = input("Please enter the patient's national code: ")
+          if patient_national_code in Patient.all_patient:
+              return patient_national_code
+          else:
+              print('No patient exists with this national code.')
+              return False
+        
+    #if the join is needed
+    def select_each_patient_info(self, patient_national_code):
+        sql_query = """
+        SELECT 
+        patient_national_code, 
+        patient_name, 
+        patient_contact_info, 
+        patient_age, 
+        patient_insurance, 
+        patients_reserved_appointments
+        FROM patients 
+        WHERE patients_reserved_appointments > 0 AND
+        patient_national_code = %s
+        """
+
+        values = ( patient_national_code)
+        return self.execute_query(sql_query, values)
+        
+    
+
+    
+
+    
+
--- main_scenario.py
+++ main_scenario.py
@@ -4,7 +4,66 @@ from Clinic import Clinic
 from Doctor import Doctor
 from Patient import Patient
 
+def logout():
+        print('You have been logged out.')
+        return None
+
 def main():
+    while True:
+        print('1. Patient')
+        print('2. Secretary')
+        print('3. Doctor')
+        position = int(input("Please choose your position: "))
+        if position == 1:
+            pass
+        elif position == 2:
+            pass
+        elif position == 3:
+            doctor = Doctor()
+            while True:
+                if not doctor.enter_code():
+                    break
+
+                print("1. View clinic table")
+                print("2. View patient data")
+                print("3. Logout")
+                option = int(input("Please choose an option: "))
+                if option == 1:
+                    clinic_id = doctor.choose_clinic()
+                    doctor.select_each_clinic_info(clinic_id)
+                elif option == 2:
+                    if not doctor.view_patient_data():
+                        break
+                    doctor.select_each_patient_info()
+                elif option == 3:
+                    doctor.logout()
+                    break
+                else:
+                    print("Invalid option. Please try again.")
+        else:
+            print("Invalid option. Please try again.")
+
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     # Get user input to determine which character they want to be
     character_choice = input("Choose a character (Doctor, Patient, Secretary): ")
     
@@ -12,7 +71,9 @@ def main():
         doctor = Doctor()
         if doctor.enter_code():
             clinic_id = doctor.choose_clinic()
-            # Continue with the scenario as the doctor
+            clinic_info = doctor.select_each_clinic_info(clinic_id)
+            print(clinic_info)
+            
     elif character_choice.lower() == "patient":
         patient_national_code = input("Enter your national code: ")
         patient_name = input("Enter your name: ")
