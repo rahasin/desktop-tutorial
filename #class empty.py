af4e8cde6e6d0bc37d9c60dacbba581a5220f017
Crusader0013 <rouzbehmalekahmadi@gmail.com>
2024-01-15 20:38:45 +0330

    drugstore scenario

--- #class empty.py
+++ #class empty.py
@@ -1,37 +1,51 @@
-#class Pharmacy
-
 class Pharmacy:
-    def __init__(self, inventory):
+
+    def __init__(self,inventory):
         # Inventory is a dictionary with drug names as keys and quantities as values
         self.inventory = inventory
 
     def dispense_drug(self, drug):
         # Check if the drug is in stock
-        if self._check_availability(drug):
-            self._update_inventory(drug)
-            print(f"Dispensed {drug}")
+        if self.check_availability(drug):
+            self.update_inventory(drug)
+            print("We have your drug in stock.\nThanks for your shopping!")
+            return True
         else:
-            print(f"Sorry, {drug} is currently out of stock.")
+            print(f"Sorry, {drug} is currently out of stock.\n")
+            return False
 
-    def _check_availability(self, drug):
+    def check_availability(self, drug):
         # Check if the drug is in stock
-        return drug in self.inventory and self.inventory[drug] > 0
+        if drug in self.inventory and self.inventory[drug] > 0 : 
+            return True
+        else : 
+            return False
 
-    def _update_inventory(self, drug):
+    def update_inventory(self, drug):
         # Dispense the drug and update the inventory
         self.inventory[drug] -= 1
 
 # Create an instance of the Pharmacy class
 inventory = {
-    'DrugA': 10,
-    'DrugB': 20,
-    'DrugC': 15,
-    'DrugD': 30
+    'drug1': 2,
+    'drug2': 20,
+    'drug3': 15,
+    'drug4': 30
 }
 pharmacy = Pharmacy(inventory)
 
 # Get the drug information
-drug = input("Enter the drug: ")
+#drug = input("Enter the drug: ")
 
 # Use the Pharmacy instance to dispense the drug
-pharmacy.dispense_drug(drug)
+#pharmacy.dispense_drug(drug)
+
+while True :
+    drug = input(f"Hello! Please Enter the drug name: {pharmacy.inventory.keys()}\n")
+    if pharmacy.dispense_drug(drug.lower()):
+        a = int(input("if you want to shop more, enter number 1.\nor if you want to leave, enter anything you want.\n"))
+        if int(a) != 1 :
+            print("Have a nice day.")
+            break
+        else : 
+            pass
